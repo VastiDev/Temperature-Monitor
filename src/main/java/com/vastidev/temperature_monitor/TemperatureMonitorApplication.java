2c1e8bf4cc7f86b4467ef70343faf9eec0a8b519
Vastiane Evelise Gonçalves <vastiane@ciandt.com>
2024-09-21 10:36:19 -0300

    first commit

---
+++ src/main/java/com/vastidev/temperature_monitor/TemperatureMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.vastidev.temperature_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemperatureMonitorApplication.class, args);
+    }
+
+}
