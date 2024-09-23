ddf5fd19cc0b344a6235d55ba6fdfb17594fd14a
VastiDev <vastiane1@gmail.com>
2024-09-22 21:38:12 -0300

    implement swagger

--- src/main/java/com/vastidev/temperature_monitor/controller/LocationController.java
+++ src/main/java/com/vastidev/temperature_monitor/controller/LocationController.java
@@ -2,8 +2,9 @@ package com.vastidev.temperature_monitor.controller;
 
 import com.vastidev.temperature_monitor.domain.Location;
 import com.vastidev.temperature_monitor.dtos.LocationDto;
-import com.vastidev.temperature_monitor.dtos.WeatherResponseDto;
 import com.vastidev.temperature_monitor.service.LocationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
@@ -17,9 +18,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/location")
 @RequiredArgsConstructor
+@Tag(name = "Location", description = "Management of Locations")
 public class LocationController {
     private final LocationService locationService;
 
+    @Operation(summary = "Create a new location", description = "Adds a new location and retrieves its current weather data")
     @PostMapping
     public ResponseEntity<Location> createLocation(@RequestBody @Valid LocationDto locationDto) {
         Location location = new Location();
@@ -28,6 +31,7 @@ public class LocationController {
         return new ResponseEntity<>(location, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get all locations", description = "Retrieves all saved locations with their weather data")
     @GetMapping
     public  ResponseEntity<List<Location>> getAllLocations() {
         List<Location> locations = locationService.getAll();
